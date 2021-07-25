02d1131bae8e60e7e886edd918e6dc28b305733e
Hadar Brayer <hadarbrayer@gmail.com>
2021-07-25 19:43:50 +0300

    create

--- inference.py
+++ inference.py
@@ -19,13 +19,9 @@ loaded_model = pickle.load(open(filename, 'rb'))
 # http://127.0.0.1:5000/predict_churn?is_male=0&num_inters=0&late_on_payment=1&age=33&years_in_contract=4.36
 @app.route('/predict_churn')
 def predict_churn():
-    if globals()['loaded_model']:
-        pass
-    else:
-        loaded_model = pickle.load(open(filename, 'rb'))
     is_male = request.args.get("is_male")
     num_inters = request.args.get("num_inters")
-    late_on_payment =request.args.get("late_on_payment")
+    late_on_payment = request.args.get("late_on_payment")
     age = request.args.get("age")
     years_in_contract = request.args.get("years_in_contract")
     data = np.array([is_male, num_inters, late_on_payment, age, years_in_contract])
@@ -36,10 +32,6 @@ def predict_churn():
 # http://127.0.0.1:5000/predict_churn_bulk
 @app.route('/predict_churn_bulk', methods=['POST'])
 def predict_churn_bulk():
-    if globals()['loaded_model']:
-        pass
-    else:
-        loaded_model = pickle.load(open(filename, 'rb'))
     bulk_data = json.loads(flask.request.get_json())
     X_test_bulk = pd.DataFrame(bulk_data)
     prediction = loaded_model.predict(X_test_bulk)
